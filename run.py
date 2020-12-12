d8e7bdaa4509e14ef172784e53d69f54bcc3e5cd
nes008 <nes008@nes008-15180.pods.nes008.svc.cluster.local>
2020-12-11 22:23:23 -0800

    final project add

--- run.py
+++ run.py
@@ -6,6 +6,14 @@ sys.path.insert(0, 'src')
 
 from build_features import create_struct
 from eda import run_model
+from feature_extraction import matrix_a
+from feature_extraction import matrix_b
+from feature_extraction import matrix_p
+from model import AAT
+from model import ABAT
+from model import APAT
+from model import APBPAT
+from model import build_svm
 
 def main(targets):
     '''
@@ -15,20 +23,58 @@ def main(targets):
     `main` runs the targets in order of data=>analysis=>model.
     '''
     if 'test' in targets:
-        data_dict = create_struct('test/testdata/')
-        print(run_model(data_dict))
-
-    if 'data' in targets:
+        print('\n')
         with open('config/data-params.json') as fh:
-            data_cfg = json.load(fh)
-        data_dict = create_struct(data_cfg['PATH'])
-    
-    if 'eda' in targets:
-        with open('config/model-params.json') as fh:
-            model_cfg = json.load(fh)
-            print("fetched data, building model features and running baseline...")
-            print(run_model(data_dict))
+            data_cfg = json.load(fh) 
+        print('creating data structure...')
+        data_dict = create_struct(data_cfg['MALWARE_PATH'], data_cfg['BENIGN_PATH'])[0]
+        malware_seen = create_struct(data_cfg['MALWARE_PATH'], data_cfg['BENIGN_PATH'])[1]
+        benign_seen = create_struct(data_cfg['MALWARE_PATH'], data_cfg['BENIGN_PATH'])[2]
+        print('creating matrices...')
+        a = matrix_a(data_dict)
+        b = matrix_b(data_dict)
+        p = matrix_p(data_dict)
+        print('creating kernels...')
+        aat = AAT(a)
+        abat = ABAT(a, b)
+        apat = APAT(a, p)
+        apbptat = APBPAT(a,p,b)
+        print('running hindroid model...')
+        print('\n')
+        print('AAT accuracy - ' + str(build_svm(aat, 'aat', malware_seen, benign_seen)[0]))
+        print('AAT f1_score - ' + str(build_svm(aat, 'aat', malware_seen, benign_seen)[1]))
+        print('ABAT accuracy - ' + str(build_svm(abat,'abat', malware_seen, benign_seen)[0]))
+        print('ABAT f1_score - ' + str(build_svm(abat, 'abat', malware_seen, benign_seen)[1]))
+        print('APAT accuracy - ' + str(build_svm(apat, 'apat', malware_seen, benign_seen)[0]))
+        print('APAT f1_score - ' + str(build_svm(apat, 'apat', malware_seen, benign_seen)[1]))
+        print('APBPTAT accuracy - ' + str(build_svm(apbptat,'apbpat', malware_seen, benign_seen)[0]))
+        print('APBPTAT f1_score - ' + str(build_svm(apbptat,'apbpat', malware_seen, benign_seen)[1]))
+        print('\n')
 
+    if 'all' in targets:
+        with open('config/data-params.json') as fh:
+            data_cfg = json.load(fh) 
+        print('creating data structure...')
+        data_dict = create_struct(data_cfg['MALWARE_PATH'], data_cfg['BENIGN_PATH'])[0]
+        malware_seen = create_struct(data_cfg['MALWARE_PATH'], data_cfg['BENIGN_PATH'])[1]
+        benign_seen = create_struct(data_cfg['MALWARE_PATH'], data_cfg['BENIGN_PATH'])[2]
+        print('creating matrices...')
+        a = matrix_a(data_dict)
+        b = matrix_b(data_dict)
+        p = matrix_p(data_dict)
+        print('creating kernels...')
+        aat = AAT(a)
+        abat = ABAT(a, b)
+        apat = APAT(a, p)
+        print('\n')
+        print('running hindroid model...')
+        print('AAT accuracy - ' + str(build_svm(aat, malware_seen, benign_seen)[0]))
+        print('AAT f1_score - ' + str(build_svm(aat, malware_seen, benign_seen)[1]))
+        print('ABAT accuracy - ' + str(build_svm(abat,'abat', malware_seen, benign_seen)[0]))
+        print('ABAT f1_score - ' + str(build_svm(abat, 'abat', malware_seen, benign_seen)[1]))
+        print('APAT accuracy - ' + str(build_svm(apat, 'apat', malware_seen, benign_seen)[0]))
+        print('APAT f1_score - ' + str(build_svm(apat, 'apat', malware_seen, benign_seen)[1]))
+        
 
 if __name__ == '__main__':
     # run via:
--- src/build_features.py
+++ src/build_features.py
@@ -8,18 +8,34 @@ from collections import defaultdict
 
 #constants
 
-def get_smali_files(smali_list, fp):
+def get_smali_files(mp,bp):
+    
     #iterate through dir 
-    for subdir, dirs, files in os.walk(fp):
+    malware_smali_list = []
+    for subdir, dirs, files in os.walk('/datasets/home/77/077/nes008/hindroid_replication/test/malware'):
         for filename in files:
             filepath = subdir + os.sep + filename
             #find smali files
             if filepath.endswith(".smali"):
-                smali_list.append(filepath)
-    print("Fetched .smali files")
+                malware_smali_list.append(filepath)
+    benign_smali_list = []
+    for subdir, dirs, files in os.walk('/datasets/home/77/077/nes008/hindroid_replication/test/benign'):
+        for filename in files:
+            filepath = subdir + os.sep + filename
+            #find smali files
+            if filepath.endswith(".smali"):
+                benign_smali_list.append(filepath)
 
+    return [malware_smali_list,benign_smali_list]
+#helper methods
+def create_set_api(def_dict, app_type, app_name):
+    def_dict[app_type][app_name]['Combined']['APIs'] = list(set(def_dict[app_type][app_name]['Combined']['APIs']))
 
-def helper(app_name, elem, num_blocks, def_dict, invoke_type):
+def create_set_package(def_dict, app_type, app_name):
+    for i in def_dict[app_type][app_name]['Packages']:
+        def_dict[app_type][app_name]['Packages'][i] = list(set(def_dict[app_type][app_name]['Packages'][i]))
+
+def helper(app_type, app_name, elem, num_blocks, def_dict, invoke_type):
     tracking_number = 'method' + str(num_blocks)
     final = re.sub(re.compile('^[^}]*}'), '', str(elem))
     api_call = final[2:]
@@ -27,35 +43,84 @@ def helper(app_name, elem, num_blocks, def_dict, invoke_type):
     api_call = apiNoParam.group(0) + str('()')
     package = re.search(re.compile('^(.*?)->'), api_call)
     try:
-        def_dict[app_name]['Packages'][package.group(1)].append(api_call)
+        def_dict[app_type][app_name]['Packages'][package.group(1)].append(api_call)
     except:
-        def_dict[app_name]['Packages'][package].append(api_call)
+        def_dict[app_type][app_name]['Packages'][package].append(api_call)
+    def_dict[app_type][app_name]['Combined']['APIs'].append(api_call)
+    def_dict[app_type][app_name]['invoke_type'][invoke_type].append(api_call)
+    def_dict[app_type][app_name]['blocks'][tracking_number].append(api_call)
 
-    def_dict[app_name]['Combined']['APIs'].append(api_call)
-    def_dict[app_name]['invoke_type'][invoke_type].append(api_call)
-    def_dict[app_name]['blocks'][tracking_number].append(api_call)
-
-def create_struct(fp):
+def create_struct(mp, bp):
     #constants
-    flag = False
+    
     num_blocks = 0
-    tracking_number = 'method' + str(num_blocks)
-    def_dict = defaultdict(lambda: defaultdict(lambda: defaultdict(list)))
-    inside_block = False
+    constant = 1
+    app_list = {}
     block_list = []
-    smali_list = []
-    get_smali_files(smali_list, fp)
-
-
+    flag = False
+    tracking_number = 'method' + str(num_blocks)
+    def_dict = defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: defaultdict(list))))
+    inside_block = False
+    count = 0
+    counter = 0
     api_list = ['invoke-static', 'invoke-virtual', 'invoke-direct', 'invoke-super', 'invoke-interface']
-    for i in smali_list:
-        f = open(i, "r")
-        app_name = i.split('/')[-2]
-        code = f.readlines()
+    app_list = []
+    malware_smali_list = get_smali_files(mp,bp)[0]
+    malware_seen = set()
+    for i in malware_smali_list:
+        file = open(i, "r")
+        app_type = 'malware'
+#     app_name = i.split('/')[7]
+        app_name = i.split('/')[9]
+
+        if app_name not in malware_seen:
+            malware_seen.add(app_name)
+        if len(malware_seen) > 40:
+            break
+        code = file.readlines()
         for index, elem in enumerate(code):
             if '.method' in elem:
                 flag = True
-        
+
+            if inside_block and ('.end method' in elem):
+                #Assign default values
+                block_list = []
+                flag = False
+                num_blocks += 1
+
+            if flag and (api_list[0] in elem):
+                helper(app_type, app_name, elem, num_blocks, def_dict, api_list[0])
+            if flag and (api_list[1] in elem):
+                helper(app_type, app_name, elem, num_blocks, def_dict, api_list[1])
+            if flag and (api_list[2] in elem):
+                helper(app_type, app_name, elem, num_blocks, def_dict, api_list[2])
+            if flag and (api_list[3] in elem):
+                helper(app_type, app_name, elem, num_blocks, def_dict, api_list[3])
+            if flag and (api_list[4] in elem):
+                helper(app_type, app_name, elem, num_blocks, def_dict, api_list[4])
+        create_set_api(def_dict, app_type, app_name)
+        create_set_package(def_dict, app_type, app_name)
+    #========Benign========#
+    count = 0
+    counter = 0 
+    api_list = ['invoke-static', 'invoke-virtual', 'invoke-direct', 'invoke-super', 'invoke-interface']
+    app_list = []
+    benign_seen = set()
+    benign_smali_list = get_smali_files(mp,bp)[1]
+    for i in benign_smali_list:
+        file = open(i, "r")
+    #     app_type = i.split('/')[4]
+    # app_name = i.split('/')[5]
+        app_name = i.split('/')[9]
+        if app_name not in benign_seen:
+            benign_seen.add(app_name)
+        if len(benign_seen) > 40:
+            break
+        code = file.readlines()
+        for index, elem in enumerate(code):
+            if '.method' in elem:
+                flag = True
+
             if inside_block and ('.end method' in elem):
                 #Assign default values
                 block_list = []
@@ -63,18 +128,19 @@ def create_struct(fp):
                 num_blocks += 1  
 
             if flag and (api_list[0] in elem): 
-                helper( app_name, elem, num_blocks, def_dict, api_list[0])
+                helper('benign', app_name, elem, num_blocks, def_dict, api_list[0])
             if flag and (api_list[1] in elem):
-                helper( app_name, elem, num_blocks, def_dict, api_list[1])
+                helper('benign', app_name, elem, num_blocks, def_dict, api_list[1])
             if flag and (api_list[2] in elem): 
-                helper(app_name, elem, num_blocks, def_dict, api_list[2])
+                helper('benign', app_name, elem, num_blocks, def_dict, api_list[2])
             if flag and (api_list[3] in elem): 
-                helper(app_name, elem, num_blocks, def_dict, api_list[3])
+                helper('benign', app_name, elem, num_blocks, def_dict, api_list[3])
             if flag and (api_list[4] in elem):
-                helper(app_name, elem, num_blocks, def_dict, api_list[4])
-
-    #return (pd.Series(def_dict).head())
-    return def_dict 
+                helper('benign', app_name, elem, num_blocks, def_dict, api_list[4])
+        create_set_api(def_dict, app_type, app_name)
+        create_set_package(def_dict, app_type, app_name)
+#     return (pd.Series(def_dict).head())
+    return [def_dict, malware_seen, benign_seen]
 
 if __name__ == '__main__':
     API_create_struct()
